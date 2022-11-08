58d8e20a05b9b033607507fe6005a40d423512ed
etorreborre <etorreborre@yahoo.com>
2022-11-08 22:29:03 +0100

    project: removed commented nix code

--- pkgs/cmt/default.nix
+++ pkgs/cmt/default.nix
@@ -15,25 +15,8 @@ with haskellPackages;
     doHaddock = false;
     doCheck = false;
 
-    # libraryToolDepends = [ hpack ];
-
-    # prePatch = "hpack";
     homepage = "https://github.com/smallhadroncollider/cmt#readme";
     description = "Write consistent git commit messages";
     license = lib.licenses.bsd3.spdxId;
     mainProgram = "cmt";
   }
-
-
-# pname = "conduit";
-#   version = "1.3.1.1";
-#   sha256 = "84dfafc92e9553c7bae4b4fe0cba3da29b37def606f88b989db95ee2dc933fa2";
-#   libraryHaskellDepends = [
-#     base bytestring directory exceptions filepath mono-traversable mtl
-#     primitive resourcet text transformers unix unliftio-core vector
-#   ];
-#   doHaddock = false;
-#   doCheck = false;
-#   homepage = "http://github.com/snoyberg/conduit";
-#   description = "Streaming data processing library";
-#   license = stdenv.lib.licenses.mit;
