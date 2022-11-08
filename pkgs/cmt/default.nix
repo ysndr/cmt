3d926d43b54edee6beab3d62400b093734635a6a
etorreborre <etorreborre@yahoo.com>
2022-11-08 22:29:03 +0100

    project: removed commented nix code

--- pkgs/cmt/default.nix
+++ pkgs/cmt/default.nix
@@ -3,7 +3,6 @@ with haskellPackages;
   mkDerivation rec {
     pname = "cmt";
     version = "0.7.1.0";
-    sha256 = "84dfafc92e9553c7bae4b4fe0cba3da29b37def606f88b989db95ee2dc933fa2";
     src = ../../.;
     isExecutable = true;
     libraryHaskellDepends = [
